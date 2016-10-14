0b2933c06ba871d78a322b1f501de7a0a8d88796
Craig Iturbe <citurbe@gmail.com>
2016-10-14 16:27:17 -0400

    create a pirate submission form

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,17 @@
 class Pirate
+  attr_accessor :name, :weight, :height
+  attr_reader :ships
+  @@PIRATES = []
+
+  def initialize (args)
+    @name = args[:name]
+    @weight = args[:weight]
+    @height = args[:height]
+    @@PIRATES << self
+
+  end
+
+  def self.all
+    @@PIRATES
+  end
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,19 @@
 class Ship
+  attr_accessor :name, :type, :booty
+  @@SHIPS = []
+
+  def initialize(args)
+    self.name = args[:name]
+    self.type = args[:type]
+    self.booty = args[:booty]
+    @@SHIPS << self
+  end
+
+  def self.all
+    @@SHIPS
+  end
+
+  def self.clear
+    @@SHIPS = []
+  end
 end
